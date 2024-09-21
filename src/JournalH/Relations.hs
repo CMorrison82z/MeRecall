1236c7a9a4588e65e5f51c7937c67268781adbde
chris <cmorrison82z@proton.me>
2024-09-21 22:11:16 +0100

    Tags are matched by `isPrefixOf` instead of just equality

--- src/JournalH/Relations.hs
+++ src/JournalH/Relations.hs
@@ -1,15 +1,15 @@
 module JournalH.Relations where
 
-import Data.List (intersect)
-import JournalH.Types (JEntriesDoc (JEntriesDoc), JournalEntry (JournalEntry, tags), Tags (Tags))
+import Data.List (intersect, intersectBy, isPrefixOf)
+import JournalH.Types (JEntriesDoc (JEntriesDoc), JournalEntry (JournalEntry, tags), Tag (Tag), Tags (Tags))
 
 -- NOTE:
 -- This checks if all `ts` are in JournalEntry tags, but the JournalEntry could have more than `ts`
 hasAllTags :: Tags -> JournalEntry -> Bool
-hasAllTags (Tags ts) (JournalEntry {tags = (Tags jts)}) = (ts ==) . intersect ts $ jts
+hasAllTags (Tags ts) (JournalEntry {tags = (Tags jts)}) = (ts ==) . intersectBy (\(Tag s1) (Tag s2) -> isPrefixOf s1 s2) ts $ jts
 
 hasAnyTags :: Tags -> JournalEntry -> Bool
-hasAnyTags (Tags ts) (JournalEntry {tags = (Tags jts)}) = not . null . intersect ts $ jts
+hasAnyTags (Tags ts) (JournalEntry {tags = (Tags jts)}) = not . null . intersectBy (\(Tag s1) (Tag s2) -> isPrefixOf s1 s2) ts $ jts
 
 filterAndTags :: Tags -> JEntriesDoc -> [JournalEntry]
 filterAndTags ts (JEntriesDoc jes) = filter (hasAllTags ts) jes
