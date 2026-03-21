311f758bb65e29e50d2a757533c51779770ac678
chris <cmorrison82z@proton.me>
2026-03-21 00:39:37 +0000

    flake

---
+++ flake.nix
@@ -0,0 +1,20 @@
+# Yeah this is irreproducible and specific to my machine. Deal with it !
+{
+    description = "Note taking app";
+
+    inputs = {
+        programming-flakes.url = "path:/home/chris/Flakes/Programming";
+    };
+
+    outputs = { programming-flakes,... }: let
+        inherit (programming-flakes.my_util) forEachSystem pkgsFor;
+    in {
+        devShells = forEachSystem (system: {
+            default = programming-flakes.haskell.standard.devShells.${system}.default.overrideAttrs ( o: {
+                nativeBuildInputs = o.nativeBuildInputs ++ (with pkgsFor.${system}; [
+                    pkg-config
+                ]);
+            });
+        });
+    };
+}
